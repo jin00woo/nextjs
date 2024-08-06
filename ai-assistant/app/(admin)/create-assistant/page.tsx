c40d3882bfd54c6980df5d6f24c02bf4d3e6f875
jin00woo <jin00woo@naver.com>
2024-08-06 23:59:43 +0900

    Created neon db.

--- ai-assistant/app/(admin)/create-assistant/page.tsx
+++ ai-assistant/app/(admin)/create-assistant/page.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+import Avatar from "@/components/Avatar";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
-function page() {
-  return <div>page</div>;
+function CreateAssistant() {
+  return (
+    <div className="flex flex-col items-center justify-center md:flex-row md:space-x-10 bg-white p-10 rounded-md m-10">
+      <Avatar seed="create-chatbot" />
+      <div>
+        <h1 className="text-xl lg:text-3xl font-semibold">Create</h1>
+        <h2 className="font-light">
+          Create a new AI Assistant to help you with your tasks
+        </h2>
+        <form className="flex flex-col md:flex-row gap-2 mt-5">
+          <Input
+            type="text"
+            required
+            placeholder="Assistant Name..."
+            className="max-w-lg"
+          />
+          <Button>Create Assistant</Button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default page;
+export default CreateAssistant;
--- ai-assistant/app/(admin)/layout.tsx
+++ ai-assistant/app/(admin)/layout.tsx
@@ -7,7 +7,7 @@ function AdminLayout({ children }: Readonly<{ children: React.ReactNode }>) {
       <Header />
       <div className="flex flex-col flex-1 lg:flex-row bg-gray-100">
         <Sidebar />
-        <div className="flex-1 flex justify-center lg:justify-start items-start max-w-5xl mx-auto w-full">
+        <div className="flex-1 flex justify-center lg:justify-center items-start max-w-5xl mx-auto w-full">
           {children}
         </div>
       </div>
